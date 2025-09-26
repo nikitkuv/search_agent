98a49d31653859d6d98ddf7988ecbfbe430b61e3
nikitkuv <nikitkuv@gmail.com>
2025-09-26 15:32:44 +0300

    fix postgresstore

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from settings import Settings
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class ChatRequest(BaseModel):
@@ -8,7 +8,9 @@ class ChatRequest(BaseModel):
     model_name: str = Settings.model_name
     temperature: float = Settings.temperature
     max_results: int = Settings.max_results
+    thread_id: Optional[str] = "default"
 
 class ChatResponse(BaseModel):
     messages: List[dict]
+    thread_id: str 
     
